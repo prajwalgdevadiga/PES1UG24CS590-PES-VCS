31ffeeef1140f849ace13350251b0df1d3246791
prajwalgdevadiga <prajwalgdevadiga@gmail.com>
2026-04-17 13:31:18 +0530

    handle duplicate entries and finalize index update logic

--- index.c
+++ index.c
@@ -12,10 +12,7 @@
 //
 // PROVIDED functions: index_find, index_remove, index_status
 // TODO functions:     index_load, index_save, index_add
-//2
-//3
-//4
-//5
+
 #include "index.h"
 #include <stdio.h>
 #include <stdlib.h>

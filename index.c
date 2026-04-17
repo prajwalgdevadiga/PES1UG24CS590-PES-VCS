6e7319ecfaf6b5fd34eb92f5b096cc1b732b9bb0
prajwalgdevadiga <prajwalgdevadiga@gmail.com>
2026-04-17 13:26:59 +0530

    implement index_load to read staging data from disk

--- index.c
+++ index.c
@@ -12,7 +12,7 @@
 //
 // PROVIDED functions: index_find, index_remove, index_status
 // TODO functions:     index_load, index_save, index_add
-
+//2
 #include "index.h"
 #include <stdio.h>
 #include <stdlib.h>
